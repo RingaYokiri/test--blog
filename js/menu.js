7d7924b5054aa2014f53d5913d18e063166fc13b
RingaYokiri <bluecrown019@gmail.com>
2026-06-15 23:41:15 +0900

    Update menu.js
    月の開閉メニュー移植

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,13 @@
-console.log("menu loaded");
+/* *.☆.* 月の導きメニュー(右上メニュー開閉) *.☆.* */
+const button = document.getElementById("menuButton");
+const menu = document.getElementById("menu");
+
+button.addEventListener("click", () => {
+
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+
+});
